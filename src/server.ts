ee8fda50c073306803a08f6fb2e6ce10ce4c483c
Daniel Silva <danielsilva@bossabox.com>
2023-09-13 12:37:56 -0300

    chore: removed old files

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import multipart from "parse-multipart-data";
+import multipart from 'parse-multipart-data';
 
 import { prisma } from "./config/database";
 
@@ -15,6 +15,44 @@ const server = Bun.serve({
       return new Response(JSON.stringify(prompts), {
         headers: { "content-type": "application/json" },
       });
+    }
+
+    if (url.pathname === "/videos" && req.method === "POST") {
+      // const rawBody = Buffer.from(await req.arrayBuffer());
+
+      // const boundary = req.headers
+      //   .get("content-type")
+      //   .split(";")[1]
+      //   .split("=")[1];
+
+      // const parts = multipart.parse(rawBody, boundary);
+
+      if (!req.headers.get('content-type')?.startsWith('multipart/form-data')) {
+        return new Response("Invalid content type", { status: 400 });
+      }
+
+      const boundary = multipart.getBoundary(req.headers.get('content-type')!);
+      const buffer = Buffer.from(await new Response(req.body).text());
+      const parts = multipart.parse(buffer, boundary);      
+      const resp = {
+        numFields: 0,
+        numFiles: 0,
+      };
+
+      for (const p of parts) {
+        console.log("Found part=", p);
+        
+        if (p.filename) {
+          resp.numFiles++;
+        } else {
+          resp.numFields++;
+        }
+      }
+      return Response.json(resp);
+
+      // return new Response(JSON.stringify(prompts), {
+      //   headers: { "content-type": "application/json" },
+      // });
     }
 
     return new Response("404!");
